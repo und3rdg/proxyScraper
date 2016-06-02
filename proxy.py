7c8caa28f306f0e6243c1b7929591293520153b7
und3rdg <und3rdg@gmail.com>
2016-06-02 16:45:00 +0100

    ip port scanner

--- proxy.py
+++ proxy.py
@@ -1,17 +1,76 @@
 #!/bin/env python3
 
-# import socket
+import socket
+import threading
+from queue import Queue
 # import urllib.request
 
-def ipRange(aStart, aEnd, bStart, bEnd, cStart, cEnd, dStart, dEnd):
-    for a in range(aStart, aEnd):
-        for b in range(bStart, bEnd):
-            for c in range(cStart, cEnd):
-                for d in range(dStart, dEnd):
-                    ipOut = str(a)+'.'+str(b)+'.'+str(c)+'.'+str(d) 
-                    print('ping '+ipOut)
-ipRange(168,169,192,193,0,255,0,255)
 
+# IP ARRAY WITH IP RANGE
+ip = []
+def ipRange(aStart, aEnd, bStart, bEnd, cStart, cEnd, dStart, dEnd):
+    for a in range(aStart, aEnd + 1):
+        for b in range(bStart, bEnd + 1):
+            for c in range(cStart, cEnd + 1):
+                for d in range(dStart, dEnd + 1):
+                    ipOut = str(a)+'.'+str(b)+'.'+str(c)+'.'+str(d) 
+                    ip.append(ipOut)
+ipRange(126,127,
+        0,10,
+        0,2,
+        0,2)
+
+# for i in ip:
+#     print('ping', i)
+
+# print('how many ip\'s: ', len(ip))
+# print(ip[262144 - 1])
+# print(ip[-1])
+
+
+
+
+# IP / PORT SCANNER
+
+print_lock = threading.Lock()
+
+port = 22
+
+def portscan(target):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        con = s.connect((target, port))
+        with print_lock:
+            print(target,':',port,'<-- IS OPEN ! ! !')
+        con.close()
+    except:
+        # print(target, ':', port, 'dead')
+        pass
+
+def threader():
+    while True:
+        worker = q.get()
+        portscan(worker)
+        q.task_done()
+
+q = Queue()
+
+# how many threads
+thNum = 30
+for x in range(thNum):
+    t = threading.Thread(target=threader)
+    t.daemon = True
+    t.start()
+
+for worker in ip:
+    q.put(worker)
+
+q.join()
+
+
+
+
+# CHECK PROXY QUALITY
 '''
 # timeout in sec
 timeout = 5
