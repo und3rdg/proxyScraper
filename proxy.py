a9b380a2c491c1d1feb036acb4cc3d9e03466de6
und3rdg <und3rdg@gmail.com>
2016-06-07 15:49:10 +0100

    generate ip range in fly

--- proxy.py
+++ proxy.py
@@ -7,21 +7,21 @@ from queue import Queue
 
 
 # IP ARRAY WITH IP RANGE
-ip = []
-def ipRange(aStart, aEnd, bStart, bEnd, cStart, cEnd, dStart, dEnd):
-    for a in range(aStart, aEnd + 1):
-        for b in range(bStart, bEnd + 1):
-            for c in range(cStart, cEnd + 1):
-                for d in range(dStart, dEnd + 1):
-                    ipOut = str(a)+'.'+str(b)+'.'+str(c)+'.'+str(d) 
-                    ip.append(ipOut)
+# ip = []
+# def ipRange(aStart, aEnd, bStart, bEnd, cStart, cEnd, dStart, dEnd):
+#     for a in range(aStart, aEnd + 1):
+#         for b in range(bStart, bEnd + 1):
+#             for c in range(cStart, cEnd + 1):
+#                 for d in range(dStart, dEnd + 1):
+#                     ipOut = str(a)+'.'+str(b)+'.'+str(c)+'.'+str(d) 
+#                     ip.append(ipOut)
 
-ipRange(5,5,
-        135,150,
-        0,255,
-        0,255)
+# ipRange(5,5,
+#         135,150,
+#         0,255,
+#         0,255)
 
-print('list Ip Done')
+# print('list Ip Done')
 
 
 
@@ -30,13 +30,28 @@ print('list Ip Done')
 print_lock = threading.Lock()
 
 openPort = []
-portRange = [80, 8080]
+# portRange = [3128, 1080, 10200, 8080]
+portRange = [80, 22]
+
+# IP range
+aStart = 5
+aEnd   = 5
+
+bStart = 0
+bEnd   = 125
+
+cStart = 0
+cEnd   = 255
+
+dStart = 0
+dEnd   = 255
 
 
 def portscan(target, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         with print_lock:
+            s.settimeout(3)
             con = s.connect((target, port))
             openPort.append( str(target) + ':' + str(port) )
             print( '     ' + openPort[-1] + ' <-- is OPEN (*)---------(*)' )
@@ -53,18 +68,26 @@ def threader():
             portscan(worker, port)
         q.task_done()
 
-# how many threads
 q = Queue()
 
+# how many threads
 thNum = 1000
 for x in range(thNum):
     t = threading.Thread(target=threader)
     t.daemon = True
     t.start()
 
-for worker in ip:
-    q.put(worker)
+# for worker in ip:
+#     q.put(worker)
 
+for a in range(aStart, aEnd + 1):
+    for b in range(bStart, bEnd + 1):
+        for c in range(cStart, cEnd + 1):
+            for d in range(dStart, dEnd + 1):
+                ipOut = str(a)+'.'+str(b)+'.'+str(c)+'.'+str(d) 
+                # ip.append(ipOut)
+                worker = ipOut
+                q.put(worker)
 q.join()
 
 print('===================== IP WITH OPEN PORTS START LIST =====================')
